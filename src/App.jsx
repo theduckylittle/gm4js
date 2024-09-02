f55bd3ddbfb27780bba8edee4cf31fc81ee60d09
Dan "Ducky" Little <theduckylittle@gmail.com>
2024-09-02 10:42:41 -0500

    Created map store and bootstrap to initialView

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,26 @@ import { SearchPanel } from "./SearchPanel";
 import axios from "axios";
 
 import { useLayerStore } from "./stores/layers";
+import { useMapStore } from "./stores/map";
 
 function App() {
   const [ready, setReady] = useState(false);
-  const parseMapbook = useLayerStore(state => state.parseMapbook);
+  const parseMapbookLayers = useLayerStore(state => state.parseMapbook);
+  const parseMapbookMap = useMapStore(state => state.parseMapbook);
 
   const [filterSet, setFilterSet] = useState({});
 
   // this is the core bootstrap of the application.
   useEffect(() => {
-    if (!ready && parseMapbook) {
+    if (!ready) {
       axios.get("./mapbook.json")
         .then(res => {
-          parseMapbook(res.data);
+          parseMapbookLayers(res.data);
+          parseMapbookMap(res.data);
           setReady(true);
         });
     }
-  }, [ready, parseMapbook]);
+  }, [ready, parseMapbookLayers, parseMapbookMap]);
 
   if (!ready) {
     return false;
